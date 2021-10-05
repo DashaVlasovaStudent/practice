62feb074819c9e468b3d216a0bd9cc3341c2e500
Vlasova <daravlasova5000@gmail.com>
2021-10-05 22:25:03 +0300

    1.2

--- src/main/java/ru/ssau/tk/dasha/practice/Mainn.java
+++ src/main/java/ru/ssau/tk/dasha/practice/Mainn.java
@@ -1,18 +1,18 @@
 package ru.ssau.tk.dasha.practice;
 
-class Mainn{
-    public static void main (String[] s){
-        Person person1 = new Person();
-        person1.setInformation("Ivan", "Ivanov", 227799);
-        person1.getInformation();
-        person1.setInformation("Petr", "Petrov", 283056);
-        person1.getInformation();
-        person1.setInformation("Konstantyn", "Viktorov", 936502);
-        person1.getInformation();
+public class Mainn {
+    public static void main(String[] args) {
+        Person person1 = new Person("Ivan", "Ivanov", 227799);
+        Person person2 = new Person("Petr", "Petrov");
+        Person person3 = new Person(936502);
+        Person person4 = new Person();
 
+        person1.getInformation();
+        person2.getInformation();
+        person3.getInformation();
+        person4.getInformation();
     }
 }
-
 class Person{
     private String firstName;
     private String lastName;
@@ -20,18 +20,30 @@ class Person{
 
 
     public Person (){
-    };
+        this.firstName = "David";
+        this.lastName = "Damiano";
+        this.passportld = 848020;
+    }
 
+    public Person (String firstName, String lastName){
+        this.firstName =firstName;
+        this.lastName = lastName;
+    }
 
-    public void setInformation (String firstName, String lastName, int passportld){
+    public Person ( int passportld){
+
+        this.passportld = passportld;
+    }
+
+    public Person (String firstName, String lastName, int passportld){
         this.firstName = firstName;
         this.lastName = lastName;
         this.passportld = passportld;
-    };
-    public void getInformation (){
+    }
+
+    public void getInformation() {
         System.out.println("Имя: " + firstName + ", фамилия: " + lastName + ", номер паспорта: " + passportld );
-    };
-
+    }/*так как выводятся сразу три параметра, то незаполненные из них будут выводиться как null, если это имя
+    или фамилия(String), и 0, если номер паспорта(int)*/
 }
-
 
