278552b3db3ac2a8c6c9179a059b6c5ecc23a097
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-12-09 21:56:42 +0400

    Гороскоп 1 часть

--- src/main/java/ru/ssau/tk/dasha/practice/Zodiac/Zodiac.java
+++ src/main/java/ru/ssau/tk/dasha/practice/Zodiac/Zodiac.java
@@ -3,8 +3,6 @@ package ru.ssau.tk.dasha.practice.Zodiac;
 import java.util.Scanner;
 
 public class Zodiac {
-    ZodiacSigns sign;
-    DaysOfWeek day;
 
     public enum ZodiacSigns {
         ARIES, //овен
@@ -32,11 +30,145 @@ public class Zodiac {
         SUNDAY
     }
 
-    public Zodiac(ZodiacSigns sign, DaysOfWeek day) {
-        this.sign = sign;
-        this.day = day;
+    public Zodiac() {
+
     }
 
+    public void createHoroscope(ZodiacSigns sign, DaysOfWeek day) {
+        switch (sign) {
+            case ARIES:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Stars are telling you to buy a christmas tree");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+
+                }
+                break;
+            case TAURUS:
+                switch (day) {
+                    case MONDAY -> System.out.println("bd");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Today is not a good to meet Yeti");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case GEMINI:
+                switch (day) {
+                    case MONDAY -> System.out.println("bq");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Today is good day to take sunbathing");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case CANCER:
+                switch (day) {
+                    case MONDAY -> System.out.println("bdd");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Be careful! Stars know there's a danger to stand on your cat");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case LEO:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("aabs");
+                    case THURSDAY -> System.out.println("HAha just have a nice day");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case VIRGO:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Time to buy New Year presents");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case LIBRA:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("You're so cool guy, just be happy and do what yo want");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case SCORPIO:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("High time to close deadlines");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+            case SAGITTARIUS:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("December is best month for you");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }break;
+
+            case AQUARIUS:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Keep pouring water(aqua)");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("xbo");
+                }break;
+            case CAPRICORN:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Christmas lights! Put them on the wall");
+                    case FRIDAY -> System.out.println("br");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }break;
+            case PISCES:
+                switch (day) {
+                    case MONDAY -> System.out.println("b");
+                    case TUESDAY -> System.out.println("bf");
+                    case WEDNESDAY -> System.out.println("bs");
+                    case THURSDAY -> System.out.println("Eat some fish");
+                    case FRIDAY -> System.out.println("bwwwr");
+                    case SATURDAY -> System.out.println("by");
+                    case SUNDAY -> System.out.println("bo");
+                }
+                break;
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -52,8 +184,12 @@ public class Zodiac {
         dayOfWeek = scanner.next();
         DaysOfWeek day = DaysOfWeek.valueOf(dayOfWeek);
 
-        Zodiac user = new Zodiac(sign, day);
+        Zodiac user = new Zodiac();
 
-        System.out.println("Your zodiac sign is "+ sign+"\n Today is (day of week) "+day);
+        System.out.println("Your horoscope:");
+
+        //System.out.println("You entered:\n zodiac sign is " + sign + "\n Today is (day of week) " + day + "\n\n");
+        user.createHoroscope(sign, day);
+
     }
 }
