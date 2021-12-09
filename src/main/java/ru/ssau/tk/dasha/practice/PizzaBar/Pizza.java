69369ecf0f91eb69707c8391a19f8bc6c188153c
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-12-10 01:21:40 +0400

    Пиццерия 3 часть (добавлена защита от неправильного ввода)

--- src/main/java/ru/ssau/tk/dasha/practice/PizzaBar/Pizza.java
+++ src/main/java/ru/ssau/tk/dasha/practice/PizzaBar/Pizza.java
@@ -25,19 +25,30 @@ public class Pizza {
         return pizza;
     }
 
+    int[] quantity = {6, 8};
+
     public static void main(String[] args) {
         PizzaType type;
         int quantityPersons;
         Pizza pizza = new Pizza();
         String type1;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("You want to order (please enter in BIG LETTERS) ");
-        type1 = scanner.next();
-        System.out.println("The quantity of people who are going to eat pizza (6 or 8)");
-        quantityPersons = scanner.nextInt();
-        type = PizzaType.valueOf(type1);
-        pizza.createPizza(pizza,type, quantityPersons);
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("The menu:\n MARGARITA\n PEPERONI\n PIZZAPIE\n\n You want to order (please enter in BIG LETTERS)\n \n ");
+            type1 = scanner.next();
+
+            System.out.println("The quantity of people who are going to eat pizza (6 or 8)");
+            quantityPersons = scanner.nextInt();
+            if ((quantityPersons != 6) & (quantityPersons != 8)){
+                throw new IllegalArgumentException();
+            }
+                type = PizzaType.valueOf(type1);
+
+            pizza.createPizza(pizza, type, quantityPersons);
+        } catch (IllegalArgumentException e) {
+            System.err.println("The pizza, you chose, isn't there in our menu OR the quantity of people entered incorrectly  ");
+        }
 
         //System.out.println("The pizza is " + type1);
 
