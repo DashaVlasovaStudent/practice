7e4bee52fe1a208300167fcf41706531c7995a18
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-12-19 19:18:51 +0400

    5.10

--- src/main/java/ru/ssau/tk/dasha/practice/Collections/CompanyModel.java
+++ src/main/java/ru/ssau/tk/dasha/practice/Collections/CompanyModel.java
@@ -1,12 +1,15 @@
 package ru.ssau.tk.dasha.practice.Collections;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 public class CompanyModel {
     Collection<Location> allLocations;
     Collection<Route> Routes;
     Collection<Driver> Drivers;
+    private int maxId;
 
     public CompanyModel() {
         allLocations = new LinkedHashSet<>();
@@ -25,4 +28,46 @@ public class CompanyModel {
     public Collection<Route> getAllRoutes() {
         return Routes;
     }
+
+    public Settlement addSettlement (String name, double latitude,double longtitude,  Location.SettlementType type, int population){
+        Settlement settlement = new Settlement();
+        settlement.setName(name);
+        settlement.setLatitude(latitude);
+        settlement.setLongitude(longtitude);
+        settlement.setType(type);
+        settlement.setSettlement(population);
+        settlement.setId(++maxId);
+        allLocations.add(settlement);
+        return settlement;
+    }
+
+    public Waypoint addWaypoint (String name, double latitude,double longtitude,  Location.WaypointType type, Settlement settlement){
+        Waypoint waypoint = new Waypoint();
+        waypoint.setName(name);
+        waypoint.setLatitude(latitude);
+        waypoint.setLongitude(longtitude);
+        waypoint.setType(type);
+        waypoint.setSettlement(settlement);
+        waypoint.setId(++maxId);
+        allLocations.add(waypoint);
+        return waypoint;
+    }
+
+    public Driver addDriver (String name, Driver.Gender gender, Date licenseExpirationDate){
+        Driver driver = new Driver();
+        driver.setName(name);
+        driver.setGender(gender);
+        driver.setLicenceExpirationDate(licenseExpirationDate);
+        driver.setId(++maxId);
+        return driver;
+    }
+
+    public  Route addRoute (List<Location> locations){
+        Route route = new Route();
+        for (Location location: locations){
+            route.addLocationAtTheEnd(location);
+        }
+        return route;
+    }
+
 }
