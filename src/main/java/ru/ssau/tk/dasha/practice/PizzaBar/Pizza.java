f4805d5b5b75915d575e0cf14aba495001341a8a
DashaVlasovaStudent <90450804+DashaVlasovaStudent@users.noreply.github.com>
2021-12-22 16:18:23 +0400

    обновление ввода

--- src/main/java/ru/ssau/tk/dasha/practice/PizzaBar/Pizza.java
+++ src/main/java/ru/ssau/tk/dasha/practice/PizzaBar/Pizza.java
@@ -4,10 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Pizza {
     enum PizzaType {
@@ -34,29 +30,7 @@ public class Pizza {
 
 
     public static void main(String[] args) {
-        PizzaType type;
-        int quantityPersons;
-        Pizza pizza = new Pizza();
-        String type1;
 
-//        try {
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("The menu:\n MARGARITA\n PEPERONI\n PIZZAPIE\n\n You want to order (please enter in BIG LETTERS)\n \n ");
-//            type1 = scanner.next();
-//
-//            System.out.println("The quantity of people who are going to eat pizza (6 or 8)");
-//            quantityPersons = scanner.nextInt();
-//            if ((quantityPersons != 6) & (quantityPersons != 8)) {
-//                throw new IllegalArgumentException();
-//            }
-//            type = PizzaType.valueOf(type1);
-//
-//            pizza.createPizza(pizza, type, quantityPersons);
-//        } catch (IllegalArgumentException e) {
-//            System.err.println("The pizza, you chose, isn't there in our menu OR the quantity of people entered incorrectly  ");
-//        }
-
-        //System.out.println("The pizza is " + type1);
         JFrame jFrame = new JFrame("PizzaBar");
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.getContentPane().setLayout(new FlowLayout());
@@ -72,13 +46,28 @@ public class Pizza {
         jFrame.getContentPane().add(button);
 
         jFrame.setVisible(true);
-
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                JOptionPane.showMessageDialog(jFrame, "Pizza is made, please take it and give us money\n Bon Apetit!");
+                if (textField.getText() != "MARGARITA" | textField.getText() != "PEPERONI" | textField.getText() != "PIZZAPIE") {
+                    JOptionPane.showMessageDialog(jFrame, "The pizza you've entered isn't there in our menu");
+                } else {
+                    JOptionPane.showMessageDialog(jFrame, "Pizza is made, please take it and give us money\n Bon Apetit!");
+                }
+
+                if (textField.getText().isEmpty()) {
+                    JOptionPane.showInputDialog("Enter the name of pizza, please");
+                    if (textField.getText() != "MARGARITA" | textField.getText() != "PEPERONI" | textField.getText() != "PIZZAPIE") {
+                        JOptionPane.showMessageDialog(jFrame, "The pizza you've entered isn't there in our menu");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(jFrame, "Pizza is made, please take it and give us money\n Bon Apetit!");
+                }
+
+
             }
         });
+
         JLabel label1 = new JLabel("\n Menu:\n MARGARITA\n PEPERONI \n PIZZAPIE");
         jFrame.getContentPane().add(label1);
     }
